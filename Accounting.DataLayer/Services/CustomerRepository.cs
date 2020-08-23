388e4209a6e8995a731645510dbdd1dab20b708b
Elham Khiabani <elham_khiabani@ymail.com>
2020-08-23 12:41:30 +0430

    Create Repository For Db

---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,86 @@
+﻿using Accounting.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Services
+{
+    class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            try
+            {
+                var customer = GetCustomerById(customerId);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch 
+            {
+
+                return false;
+            }
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomerById(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch 
+            {
+
+                return false;
+            } 
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+    }
+}
